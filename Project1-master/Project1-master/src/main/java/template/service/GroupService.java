aa77b2e0394f9f2cca974be58ba943e69ecd46ce
LamPhuss <119296534+LamPhuss@users.noreply.github.com>
2023-06-26 12:57:27 +0700

    Add files via upload

---
+++ Project1-master/Project1-master/src/main/java/template/service/GroupService.java
@@ -0,0 +1,5 @@
+package template.service;
+
+public interface GroupService {
+    void createGroup(String description ,String displayName ,String mailNickname);
+}
---
+++ Project1-master/Project1-master/src/main/java/template/service/GroupServiceImpl.java
@@ -0,0 +1,7 @@
+package template.service;
+
+public class GroupServiceImpl implements GroupService{
+    public void createGroup(String description ,String displayName ,String mailNickname){
+
+    }
+}
